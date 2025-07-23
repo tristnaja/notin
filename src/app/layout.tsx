b82c331ee933e9cd1b3a4b6504e085fdb5a29b5e
tristan <tristan.alhabas@gmail.com>
2025-07-23 11:25:03 +0700

    init: git initialization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Khula } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const khula = Khula({
+  weight: ["300", "400", "600", "700", "800"],
+  variable: "--font-khula",
   subsets: ["latin"],
 });
 
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${khula.variable} antialiased`}
       >
         {children}
       </body>
