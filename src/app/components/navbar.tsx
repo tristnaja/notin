5c5cefdd8063d9f2f71781e916d45e29771064f8
tristan <tristan.alhabas@gmail.com>
2025-07-23 12:00:07 +0700

    feat: Navbar UI

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
+
 function navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto">
-        <h1 className="text-white text-lg">Navbar</h1>
+    <>
+      <Image src="/logo-full.svg" alt="Logo" width={140} height={140} />
+      <div>
+        <ul className="flex gap-10 font-semibold">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/features">Features</a>
+          </li>
+          <li>
+            <a href="/developer">Developers</a>
+          </li>
+        </ul>
       </div>
-    </nav>
+      <div className="bg-red-alert w-max rounded-md cursor-pointer">
+        <p className="px-6 py-2 m-0 font-bold">Sign In</p>
+      </div>
+    </>
   );
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,11 @@ import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <nav>
+      <nav className="flex justify-between items-center pt-6 px-8">
         <Navbar />
       </nav>
-      <main className="">
-      </main>
-      <footer className="">
-      </footer>
+      <main className=""></main>
+      <footer className=""></footer>
     </div>
   );
 }
