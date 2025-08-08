74da89f80faf809d05682ab2bf7529acac3e5052
tristan <tristan.alhabas@gmail.com>
2025-08-08 14:47:02 +0700

    styles: Slight Adjustment

--- frontend/src/app/home/ClientHome.tsx
+++ frontend/src/app/home/ClientHome.tsx
@@ -5,21 +5,21 @@ import GenerateNoteModal from "./components/GenerateNoteModal";
 import MarkdownRenderer from "./components/MarkdownRenderer";
 
 interface ClientHomeProps {
-  content: string;
+    content: string;
 }
 
 export default function ClientHome({ content }: ClientHomeProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  return (
-    <div className="flex flex-row min-h-screen w-screen bg-black">
-      <aside className="min-w-max w-[15dvw] max-w-max justify-self-start items-center bg-grey p-6 sticky top-0 max-h-[100dvh] transition-all duration-300">
-        <Sidebar onNewNote={() => setIsModalOpen(true)} />
-      </aside>
-      <main className="p-6 flex-1">
-        <MarkdownRenderer content={content} />
-      </main>
-      <GenerateNoteModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-    </div>
-  );
+    return (
+        <div className="flex flex-row min-h-screen w-screen bg-black">
+            <aside className="min-w-max w-[15dvw] max-w-max justify-self-start items-center bg-grey p-6 sticky top-0 max-h-[100dvh] transition-all duration-300">
+                <Sidebar onNewNote={() => setIsModalOpen(true)} />
+            </aside>
+            <main className="p-23 flex-1">
+                <MarkdownRenderer content={content} />
+            </main>
+            <GenerateNoteModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+        </div>
+    );
 }
