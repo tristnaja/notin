b9a833c4d42f01449d64b0f8d5b14e08a8ad60a5
tristan <tristan.alhabas@gmail.com>
2025-09-03 21:04:15 +0700

    fix: Testing CORS Fixes

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.include_router(notes.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["https://notin.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
